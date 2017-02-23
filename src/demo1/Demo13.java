ca24e807e666dca296886a4fdee7b57267cf83fd
罗晓俊 <luoxiaojun1992@sina.cn>
2017-02-23 11:17:27 +0800

    modify

---
+++ src/demo1/Demo13.java
@@ -0,0 +1,16 @@
+package demo1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Demo13 {
+
+	public static void main(String[] args) {
+		
+		List l = new ArrayList<String>();
+		l.add(new String("demo13"));
+		System.out.println(l.get(0));
+
+	}
+
+}
