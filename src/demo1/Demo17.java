702a539164fad5a7526ac593e338a398a2c081e8
罗晓俊 <luoxiaojun1992@sina.cn>
2017-02-23 16:50:25 +0800

    modify

--- src/demo1/Demo17.java
+++ src/demo1/Demo17.java
@@ -3,11 +3,20 @@ package demo1;
 public class Demo17 implements Runnable {
 	int j;
 	
+	static int shared;
+	
 	public Demo17(int i) {
 		j = i;
 	}
 	
 	public void run() {
 		System.out.println(j);
+		modifyShared();
+	}
+	
+	private static synchronized void modifyShared() {
+		shared = shared + 1;
+		System.out.println("shared:");
+		System.out.println(shared);
 	}
 }
